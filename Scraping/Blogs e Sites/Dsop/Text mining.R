e9a51cc6caa27bc6c8b0b9f39b455e2a74448f03
Juliana Caetana <caetanajuliana@gmail.com>
2018-09-21 13:55:34 -0300

    Update Text mining.R

--- Scraping/Blogs e Sites/Dsop/Text mining.R
+++ Scraping/Blogs e Sites/Dsop/Text mining.R
@@ -12,16 +12,15 @@ library("RColorBrewer")
 library("stringr")
 library("dplyr")
 
-setwd("D:/Pós Graduação/Projeto Aplicado/Scraping/Blogs e Sites/Dsop")
+setwd("D:/PÃ³s GraduaÃ§Ã£o/Projeto Aplicado/Scraping/Blogs e Sites/Dsop")
 
-dsop <- read_csv("D:/Pós Graduação/Projeto Aplicado/Scraping/Blogs e Sites/Dsop/dsop_noticias_top10_paginas.csv", 
-                 col_types = cols(X1 = col_skip(), data = col_character()), 
+dsop <- read_csv("D:/PÃ³s GraduaÃ§Ã£o/Projeto Aplicado/Scraping/Blogs e Sites/Dsop/dsop_noticias_top10_paginas.csv", 
                  locale = locale(encoding = "WINDOWS-1252"))
 
 for (i in 1:nrow(dsop)) {
 
 text <- c(dsop$titulo[i],dsop$desc[i],dsop$categoria_principal[i])
-text <- rm_accent(text)
+text <- remove_acentos(text)
 docs <- Corpus(VectorSource(text))
 
 # toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
@@ -33,7 +32,7 @@ docs <- Corpus(VectorSource(text))
 docs <- tm_map(docs, content_transformer(tolower))
 docs <- tm_map(docs, removeNumbers)
 docs <- tm_map(docs, removeWords, stopwords("pt-br"))
-docs <- tm_map(docs, removeWords, c("… leia","nao","alem")) 
+docs <- tm_map(docs, removeWords, c("â€¦ leia","nao","alem")) 
 docs <- tm_map(docs, removePunctuation)
 docs <- tm_map(docs, stripWhitespace)
 docs <- tm_map(docs, stemDocument)
@@ -44,9 +43,9 @@ m <- as.matrix(dtm)
 v <- sort(rowSums(m),decreasing=TRUE)
 d <- data.frame(word = names(v),freq=v)
 
-max <- max(d$freq)
 
- keywords <-  toString(filter(d,d$freq == max)$word  )
+
+ keywords <-  toString(top_n(d,5,d$freq)$word  )
 
 dsop$categoria_principal[i] <- keywords
 
@@ -56,7 +55,7 @@ inspect(docs)
 
 
 
-write.csv(dsop, "dsop_noticias_top10_paginas.csv")
+write.csv(dsop, "dsop_noticias_top10_paginas.csv",row.names = F)
 
 
 
